635bcf80b9056db0e1f0c6e57c0d1052c2d4cf49
gregodiaz <diazgregoriocruz@gmail.com>
2024-12-09 15:14:02 -0300

    add: confirmation page

---
+++ src/app/confirmacion/page.tsx
@@ -0,0 +1,11 @@
+import AppLayout from '@/components/app-layout'
+
+export default function ConfirmacionPage() {
+	return (
+		<AppLayout>
+			<h1>Tu tarjeta ya se encuentra habilitada!!</h1>
+
+			<h3 className='text-center text-bold text-pretty'>¡Felicitaciones! Tu tarjeta de crédito ha sido habilitada con éxito.</h3>
+		</AppLayout>
+	)
+}
