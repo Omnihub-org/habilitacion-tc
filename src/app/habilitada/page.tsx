bb81a7cd0b8ce3a6cf10c7c91048ff4c9b1360e6
gregodiaz <diazgregoriocruz@gmail.com>
2024-12-12 10:35:07 -0300

    feat: add enabled card page

---
+++ src/app/habilitada/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+import AppLayout from '@/components/app-layout'
+import { buttonVariants } from '@/components/ui/button'
+
+export default function EnabledCardPage() {
+	return (
+		<AppLayout>
+			<h1>Tarjeta ya habilitada</h1>
+			<h3 className='text-center text-pretty'>Tu tarjeta de crédito ya se encuentra habilitada</h3>
+			<Link className={buttonVariants({ className: 'btn' })} href='/'>
+				Volver al inicio
+			</Link>
+		</AppLayout>
+	)
+}
